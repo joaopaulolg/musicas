1082b737eeade7f8052b1622a8ba608ac109b449
João Paulo L. Gonçalves <joaopaulolg@gmail.com>
2024-08-25 19:56:25 -0300

    Control Flow Migration

--- src/main/angular/src/app/gravadoras/lista-gravadoras.component.ts
+++ src/main/angular/src/app/gravadoras/lista-gravadoras.component.ts
@@ -1,36 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Gravadora } from '../shared/model/gravadora';
-import { NgFor } from '@angular/common';
 
 @Component({
     selector: 'rip-lista-gravadoras',
     templateUrl: './lista-gravadoras.component.html',
     styleUrls: ['./lista-gravadoras.component.css'],
     standalone: true,
-    imports: [NgFor]
+    imports: [],
 })
 export class ListaGravadorasComponent implements OnInit {
-  gravadoras: Gravadora[];
+    gravadoras: Gravadora[];
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-    this.gravadoras = [
-      { id: 1, nome: 'Warner Bros.', anoFundacao: 1954 },
-      { id: 2, nome: 'Nuclear Blast', anoFundacao: 1964 },
-      { id: 3, nome: 'Earache', anoFundacao: 1974 },
-    ];
-  }
+    ngOnInit(): void {
+        this.gravadoras = [
+            { id: 1, nome: 'Warner Bros.', anoFundacao: 1954 },
+            { id: 2, nome: 'Nuclear Blast', anoFundacao: 1964 },
+            { id: 3, nome: 'Earache', anoFundacao: 1974 },
+        ];
+    }
 
-  adicionar() {
-    console.log('adicionar acionado!');
-  }
+    adicionar() {
+        console.log('adicionar acionado!');
+    }
 
-  editar(idGravadora: number) {
-    console.log('editar acionado!');
-  }
+    editar(idGravadora: number) {
+        console.log('editar acionado!');
+    }
 
-  excluir(idGravadora: number) {
-    console.log('excluir acionado!');
-  }
+    excluir(idGravadora: number) {
+        console.log('excluir acionado!');
+    }
 }
