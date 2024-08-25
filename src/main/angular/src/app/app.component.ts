656982ee971b6c602c2aee6a19fee86ef943dab1
João Paulo L. Gonçalves <joaopaulolg@gmail.com>
2024-08-25 19:03:29 -0300

    Standalone Components Update

--- src/main/angular/src/app/app.component.ts
+++ src/main/angular/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: true,
+    imports: [RouterOutlet, HeaderComponent]
 })
 export class AppComponent {
-  title = 'frontend';
+    title = 'frontend';
 }
